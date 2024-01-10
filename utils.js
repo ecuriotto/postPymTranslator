3122cf7dae556e0ef3712370621a155154cf865a
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2024-01-10 11:17:20 +0100

    added colors

--- utils.js
+++ utils.js
@@ -3,13 +3,11 @@ const readlineSync = require('readline-sync');
 function updateJSONValues(obj) {
   for (const key in obj) {
     if (typeof obj[key] === 'string') {
-      console.log(`Key: ${key}, Current Value: ${obj[key]}`);
-      const answer = readlineSync.question('Do you want to update this value? (y/n): ');
-
-      if (answer.trim().toLowerCase() === 'y') {
-        const newValue = readlineSync.question(`Enter new value for ${key}: `);
-        obj[key] = newValue.trim();
-      }
+      console.log(`\x1b[33mKey: ${key}, Current Value: ${obj[key]}\x1b[0m`);
+      const newValue = readlineSync.question(
+        `\x1b[32mPress "Enter" to keep the current value or enter a new value for ${key}:\n\x1b[0m`
+      );
+      obj[key] = newValue.trim() || obj[key]; // Use the new value if entered, else keep the current value
     } else if (typeof obj[key] === 'object') {
       updateJSONValues(obj[key]); // Recursively call for nested objects
     }
